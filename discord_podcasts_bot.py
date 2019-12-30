383ac083f907f310e101b602e644e3effc5ec9af
Dave McPherson <davemcpherson@wochstudios>
2019-12-29 22:21:27 -0500

    update bot to take config file as argument

--- discord_podcasts_bot.py
+++ discord_podcasts_bot.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import getopt
 import json
 import logging
 import traceback
@@ -18,15 +20,17 @@ from cogs.error import ErrorCog
 
 description = 'A Discord call-screening bot for live radio shows.'
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print('dir_path: '+dir_path)
-with open(dir_path+'/config.json', 'r') as f:
-    config = json.load(f)
 
-TOKEN = config['AUTH']['TOKEN']
-CALLER_ROLE_ID = config['ROLES']['CALLER']['id']
-SCREENING_CHANNEL_ID = config['CHANNELS']['SCREENING']['id']
-SHOW_CHANNEL_ID = config['CHANNELS']['VOICE']['id']
+config_file = '/config.json'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+# print('dir_path: '+dir_path)
+# with open(dir_path+config_file, 'r') as f:
+#     config = json.load(f)
+
+TOKEN = ''
+CALLER_ROLE_ID = ''
+SCREENING_CHANNEL_ID = ''
+SHOW_CHANNEL_ID = ''
 
 # Below cogs represents our folder our cogs are in.
 # Following is the file name. So 'meme.py' in cogs, would be cogs.meme
@@ -41,11 +45,53 @@ logging.basicConfig(
 
 bot = commands.Bot(command_prefix='!', description=description)
 
-show_helper = podcast_utils.show_helper(bot, config)
+show_helper = None
 
-bot.add_cog(ErrorCog(bot))
-bot.add_cog(ShowCog(bot, show_helper, config))
-bot.add_cog(CallerCog(bot, show_helper, config))
+
+
+
+def parse_options(opts):
+    for opt, arg in opts:
+        if opt == '-h':
+            print('discord_podcast_bot -c <configfile>')
+            sys.exit()
+        elif opt in ("-c", "--conf"):
+            global config_file
+            config_file = arg
+            
+
+def load_config_file(config_file):
+    
+    with open(dir_path+config_file, 'r') as f:
+        config = json.load(f)
+
+    global TOKEN
+    TOKEN = config['AUTH']['TOKEN']
+    global CALLER_ROLE_ID
+    CALLER_ROLE_ID = config['ROLES']['CALLER']['id']
+    global SCREENING_CHANNEL_ID
+    SCREENING_CHANNEL_ID = config['CHANNELS']['SCREENING']['id']
+    global SHOW_CHANNEL_ID
+    SHOW_CHANNEL_ID = config['CHANNELS']['VOICE']['id']
+
+    global show_helper
+    show_helper = podcast_utils.show_helper(bot, config)
+
+    bot.add_cog(ErrorCog(bot))
+    bot.add_cog(ShowCog(bot, show_helper, config))
+    bot.add_cog(CallerCog(bot, show_helper, config))
+
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"hc:",["conf="])
+    except getopt.GetoptError:
+        print('discord_podcast_bot -c <configfile>')
+        sys.exit(2)
+    parse_options(opts)
+    load_config_file(config_file)
+    bot.run(TOKEN, bot=True, reconnect=True)
+
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # Bot Commands
@@ -78,5 +124,5 @@ async def on_voice_state_update(member, before, after):
         msg_user_voice = f"Live Caller '{show_helper.name(member)}' has joined the Live Show voice channel!"
         await screening_channel.send(msg_user_voice)
 
-
-bot.run(TOKEN, bot=True, reconnect=True)
+if __name__ == "__main__":
+    main(sys.argv[1:])
