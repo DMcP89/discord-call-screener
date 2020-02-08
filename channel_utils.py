962b1cd6eef59ad1bf7925e9d2318b6cda2eed3a
Dave McPherson <davemcpherson@wochstudios>
2020-02-08 14:55:06 -0500

    updating util scripts

--- channel_utils.py
+++ channel_utils.py
@@ -4,91 +4,78 @@ import discord
 import os
 import json
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
-with open(dir_path+'/client_config.json', 'r') as f:
-    config = json.load(f)
-    
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
 )
 
-
-CALL_IN_CHANNEL_NAME = config['CHANNELS']['CALL_IN']['name']
-CALL_IN_CHANNEL_ID = config['CHANNELS']['CALL_IN']['id']
-NONLIVE_CHANNEL_NAME = config['CHANNELS']['NONLIVE']['name']
-NONLIVE_CHANNEL_ID = config['CHANNELS']['NONLIVE']['id']
-SCREENING_CHANNEL_NAME = config['CHANNELS']['SCREENING']['name']
-SCREENING_CHANNEL_ID = config['CHANNELS']['SCREENING']['id']
-SHOW_CHANNEL_NAME = config['CHANNELS']['VOICE']['name']
-SHOW_CHANNEL_ID = config['CHANNELS']['VOICE']['id']
-HOST_ROLE_ID = config['ROLES']['HOST']['id']
-CALLER_ROLE_ID = config['ROLES']['CALLER']['id']
-
+def load_config():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(dir_path+'/client_config.json', 'r') as f:
+        logging.info("CHANNEL_UTILS LOADING client_config.json")
+        config = json.load(f)
+    return config
 
 async def channel_check(bot):
+    config = load_config()
     guild = bot.get_guild(config['GUILD'])
 
-    global CALL_IN_CHANNEL_ID
-    global NONLIVE_CHANNEL_ID
-    global SCREENING_CHANNEL_ID
-    global SHOW_CHANNEL_ID
-
-    if bot.get_channel(CALL_IN_CHANNEL_ID) is None:
+    if bot.get_channel(config['CHANNELS']['CALL_IN']['id']) is None:
         logging.info("Call in Channel Missing")
         logging.info(guild.me)
         overwrites = {
             guild.default_role: discord.PermissionOverwrite().from_pair(discord.Permissions(384064), discord.Permissions(805445649)),
             guild.me: discord.PermissionOverwrite().from_pair(discord.Permissions(384064), discord.Permissions(805445649))
         }
-        call_in_channel = await guild.create_text_channel(CALL_IN_CHANNEL_NAME, overwrites=overwrites)
-        CALL_IN_CHANNEL_ID = call_in_channel.id
+        call_in_channel = await guild.create_text_channel(config['CHANNELS']['CALL_IN']['name'], overwrites=overwrites)
+        config['CHANNELS']['CALL_IN']['id'] = call_in_channel.id
 
-    if bot.get_channel(NONLIVE_CHANNEL_ID) is None:
+    if bot.get_channel(config['CHANNELS']['NONLIVE']['id']) is None:
         logging.info("Non-live Channel Missing")
         overwrites = {
             guild.default_role: discord.PermissionOverwrite().from_pair(discord.Permissions(384064), discord.Permissions(805445649))
         }
-        non_live_channel = await guild.create_text_channel(NONLIVE_CHANNEL_NAME, overwrites=overwrites)
-        NONLIVE_CHANNEL_ID = non_live_channel.id
+        non_live_channel = await guild.create_text_channel(config['CHANNELS']['NONLIVE']['name'], overwrites=overwrites)
+        config['CHANNELS']['NONLIVE']['id'] = non_live_channel.id
 
-    if bot.get_channel(SCREENING_CHANNEL_ID) is None:
+    if bot.get_channel(config['CHANNELS']['SCREENING']['id']) is None:
         logging.info("Screening Channel Missing")
         overwrites = {
             guild.default_role: discord.PermissionOverwrite().from_pair(discord.Permissions.none(), discord.Permissions.all()),
             guild.me: discord.PermissionOverwrite().from_pair(discord.Permissions(384064), discord.Permissions(805445649)),
-            guild.get_role(HOST_ROLE_ID): discord.PermissionOverwrite().from_pair(discord.Permissions(384064), discord.Permissions(805445649))
+            #guild.get_role(config['ROLES']['HOST']['id']): discord.PermissionOverwrite().from_pair(discord.Permissions(384064), discord.Permissions(805445649))
         }
-        screening_channel = await guild.create_text_channel(SCREENING_CHANNEL_NAME, overwrites=overwrites)
-        SCREENING_CHANNEL_ID = screening_channel.id
+        screening_channel = await guild.create_text_channel(config['CHANNELS']['SCREENING']['name'], overwrites=overwrites)
+        config['CHANNELS']['SCREENING']['id'] = screening_channel.id
 
-    if bot.get_channel(SHOW_CHANNEL_ID) is None:
+    if bot.get_channel(config['CHANNELS']['VOICE']['id']) is None:
         logging.info("Show Channel Missing")
         overwrites = {
             guild.default_role:discord.PermissionOverwrite().from_pair(discord.Permissions.none(), discord.Permissions.all()),
             guild.me: discord.PermissionOverwrite().from_pair(discord.Permissions(286262288), discord.Permissions().none()),
-            guild.get_role(HOST_ROLE_ID): discord.PermissionOverwrite().from_pair(discord.Permissions(36701440), discord.Permissions().none()),
-            guild.get_role(CALLER_ROLE_ID): discord.PermissionOverwrite().from_pair(discord.Permissions(36701184), discord.Permissions().none()), 
+            guild.get_role(config['ROLES']['HOST']['id']): discord.PermissionOverwrite().from_pair(discord.Permissions(36701440), discord.Permissions().none()),
+            guild.get_role(config['ROLES']['CALLER']['id']): discord.PermissionOverwrite().from_pair(discord.Permissions(36701184), discord.Permissions().none()), 
         }
-        show_channel = await guild.create_voice_channel(SHOW_CHANNEL_NAME, overwrites=overwrites)
-        SHOW_CHANNEL_ID = show_channel.id
+        show_channel = await guild.create_voice_channel(config['CHANNELS']['VOICE']['name'], overwrites=overwrites)
+        config['CHANNELS']['VOICE']['id'] = show_channel.id
 
-    update_config_file_channel_ids()
-    await add_bot_to_channel(bot)
+    update_config_file_channel_ids(config)
+    await add_bot_to_channel(bot, config)
     return
     
-def update_config_file_channel_ids():
+def update_config_file_channel_ids(config):
     # Need to update config file with new channels
-    config['CHANNELS']['CALL_IN']['id'] = CALL_IN_CHANNEL_ID
-    config['CHANNELS']['NONLIVE']['id'] = NONLIVE_CHANNEL_ID
-    config['CHANNELS']['SCREENING']['id'] = SCREENING_CHANNEL_ID
-    config['CHANNELS']['VOICE']['id'] = SHOW_CHANNEL_ID
-    with open("config.json", "w") as jsonFile:
+    config['CHANNELS']['CALL_IN']['id'] = config['CHANNELS']['CALL_IN']['id']
+    config['CHANNELS']['NONLIVE']['id'] = config['CHANNELS']['NONLIVE']['id']
+    config['CHANNELS']['SCREENING']['id'] = config['CHANNELS']['SCREENING']['id']
+    config['CHANNELS']['VOICE']['id'] = config['CHANNELS']['VOICE']['id']
+    with open("client_config.json", "w") as jsonFile:
         json.dump(config, jsonFile)
     return
     
-async def add_bot_to_channel(bot):
+async def add_bot_to_channel(bot, config):
     bot_info = await bot.application_info()
     bot_user = bot.get_guild(config['GUILD']).get_member(bot_info.id)
     live_channel =bot.get_channel(config['CHANNELS']['VOICE']['id'])
--- role_utils.py
+++ role_utils.py
@@ -5,43 +5,37 @@ import logging
 import asyncio
 import discord
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-with open(dir_path+'/client_config.json', 'r') as f:
-    config = json.load(f)
-
-TOKEN = config['TOKEN']
-SERVER_ID = config['GUILD']
-ROLES = config['ROLES']
-
-HOST_ROLE_ID = config['ROLES']['HOST']['id']
-CALLER_ROLE_ID = config['ROLES']['CALLER']['id']
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
 )
 
+def load_config():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(dir_path+'/client_config.json', 'r') as f:
+        logging.info("ROLES UTILS LOADING client_config.json")
+        config = json.load(f)
+    return config
 
-
-def get_roles_from_server():
-    url = 'https://discordapp.com/api/guilds/'+str(SERVER_ID)+'/roles'
+def get_roles_from_server(guild_id, token):
+    url = 'https://discordapp.com/api/guilds/'+str(guild_id)+'/roles'
     headers = {
         'User-Agent': 'Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)',
-        'Authorization': 'Bot '+TOKEN
+        'Authorization': 'Bot '+token
     }
     response = requests.get(url, headers=headers)
     roles_data = json.loads(response.text)
     return roles_data
 
 
-def find_roles():
+def find_roles(config):
     caller_found = False
     host_found = False
-    for role in get_roles_from_server():
-        if role['id'] == str(ROLES['HOST']['id']):
+    for role in get_roles_from_server( config['GUILD'], config['TOKEN']):
+        if role['id'] == str(config['ROLES']['HOST']['id']):
             host_found = True
-        elif role['id'] == str(ROLES['CALLER']['id']):
+        elif role['id'] == str(config['ROLES']['CALLER']['id']):
             caller_found = True
     if caller_found and host_found:
         return {}
@@ -52,59 +46,36 @@ def find_roles():
         
 async def role_check(bot):
     logging.info("Checking if roles are available")
-    missing_roles = find_roles()
+    config = load_config()
+    missing_roles = find_roles(config)
     if missing_roles:
         logging.info("Roles are missing!")
-        await create_missing_roles(missing_roles, bot)
+        await create_missing_roles(missing_roles, bot, config)
     else:
         logging.info("All required roles are available")
         return
 
-async def create_missing_roles(missing_roles, bot):
+async def create_missing_roles(missing_roles, bot, config):
     logging.info("Creating roles...")
     # Here is were we'll handle creating anything that's missing
-    print(missing_roles)
     for role in missing_roles:
-        print(missing_roles[role])
         if not missing_roles[role]:
             role_name = config['ROLES'][role]['name']
             logging.info("Creating Role: " + role_name)
             if role == 'HOST':
-                perms = discord.PermissionOverwrite(
-                    connect=True,
-                    speak=True,
-                    mute_members=True,
-                    deafen_members=True,
-                    move_members=True,
-                    use_voice_activation=True,
-                    priority_speaker=True,
-                    read_messages=True
-                )
                 new_role = await bot.get_guild(config['GUILD']).create_role(name=role_name)
-                global HOST_ROLE_ID
-                HOST_ROLE_ID = new_role.id
+                config['ROLES']['HOST']['id'] = new_role.id
             else:
-                perms = discord.PermissionOverwrite(
-                    connect=True,
-                    speak=True,
-                    mute_members=False,
-                    deafen_members=False,
-                    move_members=False,
-                    use_voice_activation=True,
-                    priority_speaker=False,
-                    read_messages=True
-                )
                 new_role = await bot.get_guild(config['GUILD']).create_role(name=role_name)
-                global CALLER_ROLE_ID
-                CALLER_ROLE_ID = new_role.id
-            update_config_file_role_ids()
+                config['ROLES']['CALLER']['id'] = new_role.id
+            update_config_file_role_ids(config)
     return
 
-def update_config_file_role_ids():
+def update_config_file_role_ids(config):
     # Need to update config file with new roles
-    config['ROLES']['HOST']['id'] = HOST_ROLE_ID
-    config['ROLES']['CALLER']['id'] = CALLER_ROLE_ID
-    with open("config.json", "w") as jsonFile:
+    config['ROLES']['HOST']['id'] = config['ROLES']['HOST']['id']
+    config['ROLES']['CALLER']['id'] = config['ROLES']['CALLER']['id']
+    with open("client_config.json", "w") as jsonFile:
         json.dump(config, jsonFile)
     return
 # if __name__ == '__main__':
